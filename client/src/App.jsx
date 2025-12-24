703c22c45ec314f39e55d18e7d8a1ee34773f1f0
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2025-12-25 03:08:51 +0500

    Initial Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+
+function App() {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Home />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+}
+
+export default App;
