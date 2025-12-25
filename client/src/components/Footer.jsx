bbe33ef1a303f5613b312371740596f253a7aca2
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2025-12-26 03:22:10 +0500

    Movie & Movie Detail Page ui completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 
 function Footer() {
   return (
-    <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full text-gray-300">
+    <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full text-gray-300 mt-40">
       <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
         <div className="md:max-w-96">
           <img alt="" className="h-11" src={assets.logo} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
     const navigate = useNavigate();
 
   return (
-    <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
+    <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5 '>
       <Link to="/" className='max-md:flex-1'>
       <img className='w-36 h-auto' src={assets.logo} alt="" />
       </Link>
