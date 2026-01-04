2b3a9d4121b1f942d399d53a7968e6f2908c26d7
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2026-01-05 02:26:58 +0500

    User Routes Created

--- server/server.js
+++ server/server.js
@@ -1,57 +1,63 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import { connectDB } from './config/connectDB.js'
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/connectDB.js";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
-import showRouter from './routes/showRoutes.js'
-import bookingRouter from './routes/bookindRoutes.js'
-import adminRouter from './routes/adminRoutes.js'
+import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookindRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 // MongoDB Connected
-await connectDB()
+await connectDB();
 
 // Allowed Frontend Origins
-const allowedOrigins = ["http://localhost:5173"]
+const allowedOrigins = ["http://localhost:5173"];
 
 // Middlewares
-app.use(express.json())
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}))
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+app.use(clerkMiddleware());
 
 // PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // Server Route for checking
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // Inngest Route for User Creation, Deletion and Updation
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Show Movie Route from TMDB (Now-Playing)
 
-app.use("/api/show", showRouter)
+app.use("/api/show", showRouter);
 
 // Booking Routes
 
-app.use("/api/bookings", bookingRouter)
+app.use("/api/bookings", bookingRouter);
 
 // admin Routes
 
-app.use("/api/admin", adminRouter)
+app.use("/api/admin", adminRouter);
 
+// user Routes
+
+app.use("/api/user", userRouter);
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
